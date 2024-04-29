d98fb5dad8d0eeae1636dbf11e30d250d6394d23
Yassir Brhaiberh <yassir.brh46@gmail.com>
2024-04-29 01:27:27 +0100

    Creating the server.js file and setting up the server of the application.

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv').config();
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.status(200).send({message: "Let's goo !!!"})
+})
+
+mongoose
+    .connect(process.env.MONGODB_URI)
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`);
+        })
+    })
