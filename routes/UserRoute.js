800ba353cefbe8bfac953f910d4550b5324c3719
Yassir Brhaiberh <yassir.brh46@gmail.com>
2024-05-03 20:25:38 +0100

    Updating the UserRoute.js file: adding routes for profile edit handling.

--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -1,6 +1,7 @@
 // Importations
 import UserController from '../controllers/UserController';
 import authProtect from '../middlewares/AuthMiddleware';
+import upload from '../middlewares/PhotoUploadMiddleware';
 
 const express = require('express');
 // End of importations
@@ -11,6 +12,8 @@ router.post('/register', UserController.registerUser);
 router.post('/login', UserController.loginUser);
 router.get('/logout', UserController.logoutUser);
 router.get('/getuser', authProtect, UserController.getUser);
-router.get('/loggedin', UserController.loginStatus)
+router.get('/loggedin', UserController.loginStatus);
+router.post('/uploadphoto', authProtect, upload.single('photo'), UserController.uploadPhoto);
+router.get('/getphoto', authProtect, UserController.getPhoto);
 
 module.exports = router;
