31ab11f5ee2d289850e074883253933dff2c27cd
Yassir Brhaiberh <yassir.brh46@gmail.com>
2024-05-03 20:08:32 +0100

    Creating the File model: to store app files including profile pictures.

---
+++ models/FileModel.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const FileSchema = mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    type: {
+        type: String,
+        required: true
+    },
+    data: {
+        type: Buffer,
+        required: true
+    },
+    contentType: {
+        type: String,
+        required: true
+    }
+});
+
+const File = mongoose.model('File', FileSchema);
+
+module.exports = File;
