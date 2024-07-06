e1159ff79397166d6e5f4f55550f261d1ff466eb
Yassir Brhaiberh <yassir.brh46@gmail.com>
2024-07-06 20:44:47 +0100

    Update UserRoute.js: Adding new routes for updating profile data and password, also transactions...etc

--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -1,5 +1,6 @@
 // Importations
 import UserController from '../controllers/UserController';
+import TransactionController from '../controllers/TransactionController';
 import authProtect from '../middlewares/AuthMiddleware';
 import upload from '../middlewares/PhotoUploadMiddleware';
 
@@ -13,7 +14,11 @@ router.post('/login', UserController.loginUser);
 router.get('/logout', UserController.logoutUser);
 router.get('/getuser', authProtect, UserController.getUser);
 router.get('/loggedin', UserController.loginStatus);
+router.post('/updateuser', authProtect, UserController.updateUser);
+router.post('/changepassword', authProtect, UserController.changePassword);
 router.post('/uploadphoto', authProtect, upload.single('photo'), UserController.uploadPhoto);
 router.get('/getphoto', authProtect, UserController.getPhoto);
+router.get('/gettransactions', authProtect, TransactionController.getTransactions);
+router.post('/transfer', authProtect, TransactionController.transferMoney);
 
 module.exports = router;
