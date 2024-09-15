d3e5558026bcd990b78352f68395f6c0acad5fb4
Yassir Brhaiberh <yassir.brh46@gmail.com>
2024-09-15 13:55:15 +0100

    Update RequestRoute.js: Adding new route for UserController.getUsernameById named /getusernamebyid.

--- routes/RequestRoute.js
+++ routes/RequestRoute.js
@@ -1,5 +1,6 @@
 // Importations
 import AmountDemandController from '../controllers/AmountDemandController';
+import UserController from '../controllers/UserController';
 import authProtect from '../middlewares/AuthMiddleware';
 
 const express = require('express');
@@ -9,5 +10,6 @@ const router = express.Router();
 
 router.post('/requestbalance', authProtect, AmountDemandController.requestBalance);
 router.get('/getbalancerequests', authProtect, AmountDemandController.getBalanceRequests);
+router.post('/getusernamebyid', authProtect, UserController.getUsernameById);
 
 module.exports = router;
