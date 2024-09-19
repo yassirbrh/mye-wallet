e9db738635ad60062e81072d5386b8b3e17342ac
Yassir Brhaiberh <yassir.brh46@gmail.com>
2024-09-19 11:58:35 +0100

    Update RequestRoute.js: Adding two routes for /getnotifications with optional limit linked to the controller getNotifications and /seenotifications linked to the controller seeNotifications

--- routes/RequestRoute.js
+++ routes/RequestRoute.js
@@ -1,6 +1,7 @@
 // Importations
 import AmountDemandController from '../controllers/AmountDemandController';
 import UserController from '../controllers/UserController';
+import NotificationController from '../controllers/NotificationController';
 import authProtect from '../middlewares/AuthMiddleware';
 
 const express = require('express');
@@ -11,5 +12,7 @@ const router = express.Router();
 router.post('/requestbalance', authProtect, AmountDemandController.requestBalance);
 router.get('/getbalancerequests', authProtect, AmountDemandController.getBalanceRequests);
 router.post('/getusernamebyid', authProtect, UserController.getUsernameById);
+router.get('/getnotifications:limit?', authProtect, NotificationController.getNotifications);
+router.post('/seenotifications', authProtect, NotificationController.seeNotifications);
 
 module.exports = router;
