46a71f538868caeb8d26aa416dc940a8acf6b11e
Yassir Brhaiberh <yassir.brh46@gmail.com>
2024-11-04 23:45:20 +0100

    Create LandingPage.jsx: Component to hold the main components of the landing page (Header, Main and Footer)

---
+++ client/src/pages/LandingPage.jsx
@@ -0,0 +1,17 @@
+import '../styles/App.css'
+import Header from '../components/MainPages/Header'
+import Main from '../components/LandingPage/MainSection'
+import Footer from '../components/MainPages/Footer'
+
+function LandingPage() {
+
+  return (
+    <>
+        <Header />
+        <Main />
+        <Footer />
+    </>
+  )
+}
+
+export default LandingPage
