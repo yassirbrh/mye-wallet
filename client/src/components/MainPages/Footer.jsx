3e74dfda1b68f6f592cd753b0f47ddc3f8ca37ca
Yassir Brhaiberh <yassir.brh46@gmail.com>
2024-11-06 20:54:18 +0100

    Create Footer.jsx: the footer component of the main pages (Landing page, register page, login page...etc)

---
+++ client/src/components/MainPages/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+    return (
+        <>
+            <div className='footer'>
+                <div className="copy">© Copyright &amp; Reserved 2024. <a href="https://www.github.com/yassirbrh">Github:yassirbrh</a></div>
+            </div>
+        </>
+    )
+}
+
+export default Footer;
