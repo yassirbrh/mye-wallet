ccf89e96ab2ef87007b9bcb70617d6ab7bbe80ad
Yassir Brhaiberh <yassir.brh46@gmail.com>
2024-12-24 22:12:49 +0100

    Update ReportModel.js: Adding the doneAt attribute for the ReportSchema.

--- models/ReportModel.js
+++ models/ReportModel.js
@@ -9,6 +9,10 @@ const ReportSchema = mongoose.Schema({
         type: String,
         required: true
     },
+    doneAt: {
+        type: Date,
+        required: true
+    },
     answer: {
         type: String,
         required: false
