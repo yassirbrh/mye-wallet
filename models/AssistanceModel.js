e15a21c9d7e6bd65432edf7f84e986b8b74450c9
Yassir Brhaiberh <yassir.brh46@gmail.com>
2025-03-03 15:34:26 +0000

    Update AssistanceModel.js: add the reference to User model in the userID attribute

--- models/AssistanceModel.js
+++ models/AssistanceModel.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const AssistanceSchema = mongoose.Schema({
     userID: {
         type: mongoose.Types.ObjectId,
+        ref: "User",
         required: true
     },
     messages: [{
