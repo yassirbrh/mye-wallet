508e47876742e80dff39c26f7e9f5bdbc450098d
Yassir Brhaiberh <yassir.brh46@gmail.com>
2025-03-03 15:40:59 +0000

    Update AmountDemandModel.js: add the reference to User model in the userID attribute

--- models/AmountDemandModel.js
+++ models/AmountDemandModel.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const AmountDemandSchema = mongoose.Schema({
     userID: {
         type: mongoose.Types.ObjectId,
+        ref: "User",
         required: true
     },
     amount: {
