d46925d79b141120373ff7b72f116eb68d76452d
Yassir Brhaiberh <yassir.brh46@gmail.com>
2025-03-13 14:37:02 +0000

    Create AuthCheck.jsx: Implement authentication check and page redirection based on user login status

---
+++ admin/src/auth/AuthCheck.jsx
@@ -0,0 +1,52 @@
+// AuthCheck.jsx
+import React, { useEffect, useState } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
+import axios from 'axios';
+
+function AuthCheck({ children }) {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    axios
+      .get('/api/admin/loggedin')
+      .then((response) => {
+        if (response.data === true) {
+          // If the user is authenticated, navigate based on the current page
+          const authenticatedPages = [
+            'OverviewPage',
+            'AdminPage',
+            'UserPage',
+            'ReportPage',
+            'AmountDemandPage',
+            'CreditCardPage',
+            'AssistancePage',
+            'ProfilePage'
+          ];
+          if (!authenticatedPages.includes(children.type.name)) {
+            navigate('/overview', { replace: true });
+          }
+        } else {
+          // If not authenticated, handle navigation based on the page type
+          //const homePages = ['LandingPage', 'RegisterPage', 'LoginPage'];
+          if (children.type.name !== 'LandingPage') {
+            navigate('/', { replace: true });
+          }
+        }
+      })
+      .catch((error) => {
+        console.error('Error fetching login status:', error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, [navigate, children]);
+
+  if (loading) return null; // Avoid rendering anything while loading
+
+  // If loading is complete, render the children as intended
+  return children;
+}
+
+export default AuthCheck;
